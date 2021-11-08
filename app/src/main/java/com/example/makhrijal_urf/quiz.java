e7f6715f7e972752cfeeebe417c3e49f3870fb5c
Hasher001 <hasherrajana@outlook.com>
2021-11-09 01:40:20 +0500

    quiz generation

--- app/src/main/java/com/example/makhrijal_urf/quiz.java
+++ app/src/main/java/com/example/makhrijal_urf/quiz.java
@@ -3,12 +3,23 @@ package com.example.makhrijal_urf;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import android.view.View;
+import java.util.*;
+import java.io.*;
 
 public class quiz extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+
+    public void quizz(View V){
+
+
     }
 }
