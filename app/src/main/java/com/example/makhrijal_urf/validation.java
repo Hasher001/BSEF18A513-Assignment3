06ece6e7ac4b305599d3b5246611a6455378ccd9
Hasher001 <hasherrajana@outlook.com>
2021-11-09 22:06:28 +0500

    All done

--- app/src/main/java/com/example/makhrijal_urf/validation.java
+++ app/src/main/java/com/example/makhrijal_urf/validation.java
@@ -2,11 +2,16 @@ package com.example.makhrijal_urf;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
-import android.os.Bundle;
 import android.view.View;
-
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import java.lang.*;
+import java.util.*;
+import java.io.*;
 
 public class validation extends AppCompatActivity {
 
@@ -15,12 +20,10 @@ public class validation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_validation);
     }
-    public void openWebPage(View v) {
-           Uri webpage = Uri.parse("https://github.com/Hasher001/BSEF18A513-Assignment3");
-           Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
-           startActivity(intent);
+    public void openWebPage(View V) {
+            Uri webpage = Uri.parse("https://github.com/Hasher001/BSEF18A513-Assignment3");
+            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+            startActivity(intent);
     }
-
-
 
 }
