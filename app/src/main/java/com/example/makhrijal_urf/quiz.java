80cc2c6125a843fd393dd4289965ca3ea6decb30
Hasher001 <hasherrajana@outlook.com>
2021-11-09 17:13:12 +0500

    quiz update

--- app/src/main/java/com/example/makhrijal_urf/quiz.java
+++ app/src/main/java/com/example/makhrijal_urf/quiz.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.content.Intent;
 import android.net.Uri;
 import android.view.View;
+import android.widget.TextView;
+import java.lang.Math;
 import java.util.*;
 import java.io.*;
 
@@ -16,10 +18,38 @@ public class quiz extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+
+
+        //Creating arrays for different portions of questions
+        String[] Arr1={"Sound Produced from End of Throat ?","Sound Produced from Middle of Throat ?","Sound Produced from Start of the Throat ?"};
+        String[] Arr2={"Sound Produced from Base of Tongue which is near Uvula touching the mouth roof ?","Sound Produced from Portion of Tongue near its base touching the roof of mouth?"};
+        String[] Arr3={"Sound Produced from Tongue touching the center of the mouth roof ?","Sound Produced from One side of the tongue touching the molar teeth ?"};
+
+
+        //Generating random numbers to shuffle questions each time
+        View st1 = (TextView)findViewById(R.id.statement1);
+        Random random = new Random();
+        int value1 = random.nextInt(2 + 0) + 0;
+
+        //setting text value to randomly generated question
+        ((TextView) st1).setText(Arr1[value1]);
+
+
+        View st2 = findViewById(R.id.statement2);
+        View st3 = findViewById(R.id.statement3);
+
+
+
     }
 
-    public void quizz(View V){
-
-
+    public void back_button(View v) {
+        switch(v.getId()) {
+            case R.id.Back:
+                Intent intent =  new Intent(quiz.this, MainActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
+
+
 }
