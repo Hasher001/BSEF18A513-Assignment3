2921a98cd789630e3f97f97108416bb6d2d6b1fe
Hasher001 <hasherrajana@outlook.com>
2021-12-07 23:49:23 +0500

    Menu bar finalize

--- app/src/main/java/com/example/makhrijal_urf/MainActivity.java
+++ app/src/main/java/com/example/makhrijal_urf/MainActivity.java
@@ -6,7 +6,9 @@ import androidx.appcompat.widget.Toolbar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,32 +30,61 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void learn_button(View v) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.Learnn:
+                Intent intent = new Intent(MainActivity.this, Learn.class);
+                startActivity(intent);
+                break;
+
+
+            case R.id.Quizz:
+                Intent intent2 = new Intent(MainActivity.this, quiz.class);
+                startActivity(intent2);
+                break;
+
+
+            case R.id.Validationn:
+                Intent intent3 = new Intent(MainActivity.this, validation.class);
+                startActivity(intent3);
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
+
+
+    public boolean Learn_button(View v){
         switch(v.getId()) {
             case R.id.Learn:
                 Intent intent =  new Intent(MainActivity.this, Learn.class);
                 startActivity(intent);
                 break;
         }
+        return true;
     }
 
-    public void valid_button(View v) {
-        switch(v.getId()) {
-            case R.id.Validation:
-                Intent intent =  new Intent(MainActivity.this, validation.class);
-                startActivity(intent);
-                break;
+        public void valid_button(View v) {
+            switch(v.getId()) {
+                case R.id.Validation:
+                    Intent intent =  new Intent(MainActivity.this, validation.class);
+                    startActivity(intent);
+                    break;
+            }
         }
-    }
 
-    public void quiz_button(View v) {
-        switch(v.getId()) {
-            case R.id.Quiz:
-                Intent intent =  new Intent(MainActivity.this, quiz.class);
-                startActivity(intent);
-                break;
+        public void quiz_button(View v) {
+            switch(v.getId()) {
+                case R.id.Quiz:
+                    Intent intent =  new Intent(MainActivity.this, quiz.class);
+                    startActivity(intent);
+                    break;
+
+            }
         }
-    }
 
 
-}
+        }
