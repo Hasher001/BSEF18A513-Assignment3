8776bead443dac20c57b81e4b22e53f574e2c4b6
Hasher001 <hasherrajana@outlook.com>
2021-12-07 23:00:54 +0500

    Menu bar added with items

--- app/src/main/java/com/example/makhrijal_urf/MainActivity.java
+++ app/src/main/java/com/example/makhrijal_urf/MainActivity.java
@@ -1,17 +1,31 @@
 package com.example.makhrijal_urf;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar3);
+        setSupportActionBar(toolbar);
+
     }
 
     public void learn_button(View v) {
